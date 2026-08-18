e7baf7bbe01b8dcc92a51717477a8e201dc7f725
inpeacedTeams <egorzzvv@gmail.com>
2026-08-18 16:56:39 +0300

    chore: load canvas renderer adapter

--- modules/canvas-renderer-ui.js
+++ modules/canvas-renderer-ui.js
@@ -2,11 +2,9 @@
 (function(){
  if(!window.DrawForgeCanvasRenderer)return;
  var R=window.DrawForgeCanvasRenderer;
- var originalPaintShape=window.paintShape;
  window.paintShape=function(el,selected,preview){
   var renderer=R.create({context:window.ctx,zoom:window.S&&window.S.viewport?window.S.viewport.z:1,selected:function(){return selected},format:function(v){return window.fmtVal?window.fmtVal(v):Math.round(v)+' мм'}});
-  renderer.shape(el,preview);
-  if(el.type==='dimension')renderer.dimension(el);
+  if(el.type==='dimension')renderer.dimension(el);else renderer.shape(el,preview);
  };
  window.DrawForgeModules=window.DrawForgeModules||{};window.DrawForgeModules.canvasRenderer=R;
 })();
